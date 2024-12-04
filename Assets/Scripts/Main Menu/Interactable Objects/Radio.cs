1ef4c6ce729f368ea4227f27434324b889341f96
Óscar Alarcón <oscaral566@gmail.com>
2024-12-04 21:30:02 +0100

    inicio

--- Assets/Scripts/Main Menu/Interactable Objects/Radio.cs
+++ Assets/Scripts/Main Menu/Interactable Objects/Radio.cs
@@ -5,24 +5,18 @@ using UnityEngine.EventSystems;
 
 public class Radio : AInteractableObject
 {
-    [SerializeField] private GameObject _ButtonLobby;
-    public override void EnableInteraction()
-    {
-        _isEnabled = true;
-        _ButtonLobby.SetActive(true);
-    }
-
-    public override void DisableInteraction()
-    {
-        _isEnabled = false;
-        _ButtonLobby.SetActive(false);
-        gameObject.transform.localScale /= scaleIncrease;
-
-    }
-
     public override void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("radio clicada");
+        throw new System.NotImplementedException();
     }
-   
+
+    public void OnButtonCreateLobby()
+    {
+        // logica pulsado boton de crear lobby
+    }
+
+    public void OnButtonJoinLobby()
+    {
+        // join lobby
+    }
 }
