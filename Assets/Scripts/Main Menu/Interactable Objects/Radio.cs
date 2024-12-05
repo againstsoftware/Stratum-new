0fba64546104295378d2e147e7b69e526a9656f4
Óscar Alarcón <oscaral566@gmail.com>
2024-12-05 21:23:37 +0100

    ya casi radio

--- Assets/Scripts/Main Menu/Interactable Objects/Radio.cs
+++ Assets/Scripts/Main Menu/Interactable Objects/Radio.cs
@@ -3,31 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-
 public class Radio : AInteractableObject
 {
-    [SerializeField] private List<GameObject> radioWheel;
+    [SerializeField] private List<GameObject> _radioWheels;
     [SerializeField] private LobbyInteraction _lobbyInteraction;
+
+    private List<float> _targetRot;
+    private List<float> _currentRot;
+    private float rotSpeed = 100f;
+    private bool _WheelsMoving = false;
+
+    private void Start()
+    {
+        _targetRot = new List<float>();
+        _currentRot = new List<float>();
+    }
+
     public override void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("radio pulsada");
+        Debug.Log("Radio pulsada");
+    }
+
+    private void Update()
+    {
+        if (_WheelsMoving)
+        {
+            for (int i = 0; i < _radioWheels.Count; i++)
+            {
+                if (_radioWheels[i] != null && (_targetRot[i] - _currentRot[i] > 0.1f))
+                {
+                    float step = Mathf.Min(Time.deltaTime * rotSpeed, _targetRot[i] - _currentRot[i]); 
+                    _radioWheels[i].transform.rotation = Quaternion.AngleAxis(step, Vector3.forward) * _radioWheels[i].transform.rotation;
+                    _currentRot[i] += step;
+                }
+            }
+
+            if (AllWheelsStopped())
+            {
+                _WheelsMoving = false;
+            }
+        }
+    }
+
+    private bool AllWheelsStopped()
+    {
+        for (int i = 0; i < _targetRot.Count; i++)
+        {
+            if (Mathf.Abs(_targetRot[i] - _currentRot[i]) > 0.1f)
+                return false;
+        }
+        return true;
     }
 
     public void OnButtonCreateLobby()
     {
-        // logica pulsado boton de crear lobby
-            // girar ruedas
-            // mostrar código de sala
+        Debug.Log("Crear lobby pulsado");
 
-        Debug.Log("crear lobby pulsado");
+        if (_lobbyInteraction != null)
+        {
+            _lobbyInteraction.HostButton();
+        }
 
-        _lobbyInteraction.HostButton();
+        _targetRot.Clear();
+        _currentRot.Clear();
 
-        
+        for (int i = 0; i < _radioWheels.Count; i++)
+        {
+            if (_radioWheels[i] != null)
+            {
+                _targetRot.Add(Random.Range(45f, 270f));
+                _currentRot.Add(0f);
+            }
+        }
+
+        _WheelsMoving = true;
     }
 
     public void OnButtonJoinLobby()
     {
-        // join lobby
+        // Join lobby
+        Debug.Log("Join lobby pulsado");
     }
 }
